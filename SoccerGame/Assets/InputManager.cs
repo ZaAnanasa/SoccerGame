ada7018a35079589a33ee96c8f2ec42ff574fea4
ZaAnanasa <90453612+ZaAnanasa@users.noreply.github.com>
2022-04-02 15:23:19 +0200

    wut

---
+++ SoccerGame/Assets/InputManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private TouchControls touchControls;
+
+    public delegate void StartTouchEvent(Vector2 position, float time);
+    public event StartTouchEvent OnStartTouch;
+    public delegate void EndTouchEvent(Vector2 position, float time);  
+    public event EndTouchEvent OnEndTouch;
+
+
+    private void Awake(){
+        touchControls = new TouchControls();
+    }
+    private void OnEnable(){
+        touchControls.Enable();
+    }
+    private void OnDisable(){
+        touchControls.Disable();
+    }
+    private void Start(){
+        touchControls.Touch.TouchPress.started += ctx => StartTouch(ctx);
+        touchControls.Touch.TouchPress.canceled += ctx => EndTouch(ctx);
+    }
+
+    private void StartTouch(InputAction.CallbackContext context)
+    {
+        Debug.Log("Touch Started: " + touchControls.Touch.TouchPosition.ReadValue<Vector2>());
+        if ( OnStartTouch != null){
+            OnStartTouch(touchControls.Touch.TouchPosition.ReadValue<Vector2>(), (float)context.startTime);
+        }
+    }
+    private void EndTouch(InputAction.CallbackContext context)
+    {
+          Debug.Log("Touch Ended: " );
+          if ( OnEndTouch != null){
+            OnEndTouch(touchControls.Touch.TouchPosition.ReadValue<Vector2>(), (float)context.time);
+        }
+    }
+}
+
